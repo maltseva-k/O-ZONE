fb8a2d1e05ab7b04a82d0c53134d8c0fa74e5f40
maltseva-k <k.obydennova@gmail.com>
2022-06-02 10:56:27 +0300

    delete console.log in getData.js

--- src/modules/getData.js
+++ src/modules/getData.js
@@ -1,7 +1,6 @@
 const getData = () => {
     return fetch('https://o-zone-c0bda-default-rtdb.firebaseio.com/goods.json')
                 .then((response) => response.json())
-                .then((data)=>console.log(data))
 }
 
 export default getData
