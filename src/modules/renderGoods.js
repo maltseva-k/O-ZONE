ed57cd3c567849ba67cae81b18f49496361fc21b
maltseva-k <k.obydennova@gmail.com>
2022-06-03 08:46:25 +0300

    add 'hot sale' div

--- src/modules/renderGoods.js
+++ src/modules/renderGoods.js
@@ -7,17 +7,18 @@ const renderGoods = (goods) => {
 
         goodsWrapper.insertAdjacentHTML('beforeend',
             `<div class="card">
-                <div class="card-img-wrapper">
-                    <span class="card-img-top"
-                          style="background-image: url('${goodsItem.img}')">
-                    </span>
+                    ${goodsItem.sale ? '<div class="card-sale">🔥Hot Sale🔥</div>' : ''}
+                    <div class="card-img-wrapper">
+                        <span class="card-img-top"
+                              style="background-image: url('${goodsItem.img}')">
+                        </span>
+                    </div>
+                    <div class="card-body justify-content-between">
+                        <div class="card-price">${goodsItem.price} ₽</div>
+                        <h5 class="card-title">${goodsItem.title}</h5>
+                        <button class="btn btn-primary">В корзину</button>
+                    </div>
                 </div>
-                <div class="card-body justify-content-between">
-                    <div class="card-price">${goodsItem.price} ₽</div>
-                    <h5 class="card-title">${goodsItem.title}</h5>
-                    <button class="btn btn-primary">В корзину</button>
-                </div>
-            </div>
         `)
     })
 }
